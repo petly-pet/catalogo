91ad1932b38ea5f0b80c2293efe88fbf5ec15fe8
hellewd <howdy.mowdy4@gmail.com>
2024-12-18 15:02:29 -0300

    00001

---
+++ main.js
@@ -0,0 +1,31 @@
+/**
+ * Cargar una sección dinámica desde un archivo HTML.
+ * @param {string} sectionName - Nombre de la sección a cargar.
+ */
+async function showSection(sectionName) {
+    const sectionContainer = document.getElementById("sectionContainer");
+    const sectionFile = `sections/${sectionName.toLowerCase()}.html`;
+
+    try {
+        const response = await fetch(sectionFile);
+        if (response.ok) {
+            const content = await response.text();
+            sectionContainer.innerHTML = content;
+        } else {
+            sectionContainer.innerHTML = `<p class="text-red-500 text-center">Error: No se pudo cargar la sección ${sectionName}.</p>`;
+        }
+    } catch (error) {
+        sectionContainer.innerHTML = `<p class="text-red-500 text-center">Error de red al intentar cargar la sección.</p>`;
+        console.error(`Error al cargar ${sectionFile}:`, error);
+    }
+}
+
+
+function updatePrice(productId, originalPrice, price) {
+    const originalPriceElement = document.getElementById(`original-price-${productId}`);
+    const priceElement = document.getElementById(`price-${productId}`);
+
+    originalPriceElement.textContent = `$${originalPrice.toFixed(3)}`;
+    priceElement.textContent = `$${price.toFixed(3)}`;
+}
+
