aa831f32fa289adfbc3da6cc161d3c7e29bd06aa
hellewd <howdy.mowdy4@gmail.com>
2024-12-18 01:21:26 -0300

    boc

--- src/main.js
+++ src/main.js
@@ -8,3 +8,11 @@ function showSection() {
 
     document.getElementById(selected).classList.remove("hidden");
 }
+
+function updatePrice(productId, originalPrice, price) {
+    const originalPriceElement = document.getElementById(`original-price-${productId}`);
+    const priceElement = document.getElementById(`price-${productId}`);
+    
+    originalPriceElement.textContent = `$${originalPrice.toFixed(3)}`;
+    priceElement.textContent = `$${price.toFixed(3)}`;
+}
