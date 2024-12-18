0463fcaef924174728ef85dc5298851816abb03a
hellewd <howdy.mowdy4@gmail.com>
2024-12-18 16:17:38 -0300

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "index.html",
     "./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ...colors, // Esto extiende y carga todos los colores
+      },
+    },
   },
   plugins: [],
 }
